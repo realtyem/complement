5428de965559ecc8d09f753d3a2b5daa7eaaa289
Jason Little <realtyem@gmail.com>
2022-12-30 03:50:26 -0600

    [msc3787_test.go] Regroup subtests so that tests that use same Blueprints run sequentially, allowing full parallelization.

--- tests/msc3787_test.go
+++ tests/msc3787_test.go
@@ -21,28 +21,42 @@ var (
 )
 
 func TestMSC3787(t *testing.T) {
-	t.Run("KnockingInMSC3787Room", func(t *testing.T) {
-		//t.Parallel()
-		//TestKnockingInMSC3787Room
-		// See TestKnocking
-		doTestKnocking(t, msc3787RoomVersion, msc3787JoinRule)
+	t.Run("Subgroup 1", func(t *testing.T) {
+		t.Parallel()
+		// uses BlueprintAlice
+		t.Run("KnockRoomsInPublicRoomsDirectoryInMSC3787Room", func(t *testing.T) {
+			// See TestKnockRoomsInPublicRoomsDirectory(in knocking_test.go)
+			// TestKnockRoomsInPublicRoomsDirectoryInMSC3787Room
+			// with users: alice
+			doTestKnockRoomsInPublicRoomsDirectory(t, msc3787RoomVersion, msc3787JoinRule)
+		})
+		t.Run("CannotSendKnockViaSendKnockInMSC3787Room", func(t *testing.T) {
+			// See TestCannotSendKnockViaSendKnock(in federation_room_join_test.go)
+			// TestCannotSendKnockViaSendKnockInMSC3787Room
+			// with users: alice and fake user charlie
+			testValidationForSendMembershipEndpoint(t, "/_matrix/federation/v1/send_knock", "knock",
+				map[string]interface{}{
+					"preset":       "public_chat",
+					"room_version": msc3787RoomVersion,
+				},
+			)
+		})
 	})
-	t.Run("KnockRoomsInPublicRoomsDirectoryInMSC3787Room", func(t *testing.T) {
-		//t.Parallel()
-		// See TestKnockRoomsInPublicRoomsDirectory
-		// TestKnockRoomsInPublicRoomsDirectoryInMSC3787Room
-		doTestKnockRoomsInPublicRoomsDirectory(t, msc3787RoomVersion, msc3787JoinRule)
-	})
-	t.Run("CannotSendKnockViaSendKnockInMSC3787Room", func(t *testing.T) {
-		//t.Parallel()
-		// See TestCannotSendKnockViaSendKnock
-		// TestCannotSendKnockViaSendKnockInMSC3787Room
-		testValidationForSendMembershipEndpoint(t, "/_matrix/federation/v1/send_knock", "knock",
-			map[string]interface{}{
-				"preset":       "public_chat",
-				"room_version": msc3787RoomVersion,
-			},
-		)
+	t.Run("Subgroup 2", func(t *testing.T) {
+		t.Parallel()
+		// uses BlueprintFederationTwoLocalOneRemote
+		t.Run("KnockingInMSC3787Room", func(t *testing.T) {
+			// TestKnockingInMSC3787Room
+			// See TestKnocking(in knocking_test.go)
+			// with users: alice, bob, charlie and fake user derek
+			doTestKnocking(t, msc3787RoomVersion, msc3787JoinRule)
+		})
+		t.Run("RestrictedRoomsRemoteJoinLocalUserInMSC3787Room", func(t *testing.T) {
+			// TestRestrictedRoomsRemoteJoinLocalUserInMSC3787Room
+			// See TestRestrictedRoomsRemoteJoinLocalUser(in restricted_rooms_test.go)
+			// with users: alice, bob, charlie
+			doTestRestrictedRoomsRemoteJoinLocalUser(t, msc3787RoomVersion, msc3787JoinRule)
+		})
 	})
 	t.Run("RestrictedRoomsLocalJoinInMSC3787Room", func(t *testing.T) {
 		t.Parallel()
@@ -76,16 +90,12 @@ func TestMSC3787(t *testing.T) {
 		// Execute the checks.
 		checkRestrictedRoom(t, alice, bob, allowed_room, room, msc3787JoinRule)
 	})
-	t.Run("RestrictedRoomsRemoteJoinLocalUserInMSC3787Room", func(t *testing.T) {
-		t.Parallel()
-		// See TestRestrictedRoomsRemoteJoinLocalUser
-		// TestRestrictedRoomsRemoteJoinLocalUserInMSC3787Room
-		doTestRestrictedRoomsRemoteJoinLocalUser(t, msc3787RoomVersion, msc3787JoinRule)
-	})
 	t.Run("RestrictedRoomsRemoteJoinFailOverInMSC3787Room", func(t *testing.T) {
 		t.Parallel()
 		// See TestRestrictedRoomsRemoteJoinFailOver
 		// TestRestrictedRoomsRemoteJoinFailOverInMSC3787Room
+		// uses custom Blueprint with 3 homeservers
+		// with users: alice:hs1, bob:hs2, charlie:hs3
 		doTestRestrictedRoomsRemoteJoinFailOver(t, msc3787RoomVersion, msc3787JoinRule)
 	})
 }
