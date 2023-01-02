aa00d3df0e128be98893c7d8f440e605b03f9122
Jason Little <realtyem@gmail.com>
2023-01-02 06:42:55 -0600

    [federation_query_profile_test.go] Refactor tests.
    1. Wrap all tests in t.Run().
    2. Share deployment among all tests.
    3. Parallel tests.
    
    Original Test time: 69 seconds(including 20 for blueprints and 16 for containers)
    Sequential Test time: 55 seconds(including 20 for blueprints and 8 for containers)
    Parallel Test time: 44 seconds(including 20 for blueprints and 7 for containers)

--- tests/federation_query_profile_test.go
+++ tests/federation_query_profile_test.go
@@ -18,117 +18,124 @@ import (
 // TODO:
 // Inbound federation can query profile data
 
-// Test that the server can make outbound federation profile requests
-// https://matrix.org/docs/spec/server_server/latest#get-matrix-federation-v1-query-profile
-func TestOutboundFederationProfile(t *testing.T) {
+func TestFederationProfile(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintAlice)
 	defer deployment.Destroy(t)
 
-	srv := federation.NewServer(t, deployment,
-		federation.HandleKeyRequests(),
-	)
-	cancel := srv.Listen()
-	defer cancel()
+	t.Run("parallel", func(t *testing.T) {
+		t.Run("Outbound", func(t *testing.T) {
+			t.Parallel()
+			// Test that the server can make outbound federation profile requests
+			// https://matrix.org/docs/spec/server_server/latest#get-matrix-federation-v1-query-profile
+			// formerly TestOutboundFederationProfile
+			srv := federation.NewServer(t, deployment,
+				federation.HandleKeyRequests(),
+			)
+			cancel := srv.Listen()
+			defer cancel()
 
-	// sytest: Outbound federation can query profile data
-	t.Run("Outbound federation can query profile data", func(t *testing.T) {
-		remoteUserID := srv.UserID("user")
-		remoteDisplayName := "my remote display name"
+			// sytest: Outbound federation can query profile data
+			// users needed: "fake" @user_id and an unauthenticated Client
+			t.Run("Outbound federation can query profile data", func(t *testing.T) {
+				remoteUserID := srv.UserID("user")
+				remoteDisplayName := "my remote display name"
 
-		srv.Mux().Handle("/_matrix/federation/v1/query/profile", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			userID := req.URL.Query().Get("user_id")
-			if userID != remoteUserID {
-				w.WriteHeader(500)
-				t.Fatalf("GET /_matrix/federation/v1/query/profile with wrong user ID, got '%s' want '%s'", userID, remoteUserID)
-				return
-			}
-			resBody, err := json.Marshal(struct {
-				Displayname string `json:"displayname"`
-				AvatarURL   string `json:"avatar_url"`
-			}{
-				remoteDisplayName, "",
+				srv.Mux().Handle("/_matrix/federation/v1/query/profile", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+					userID := req.URL.Query().Get("user_id")
+					if userID != remoteUserID {
+						w.WriteHeader(500)
+						t.Fatalf("GET /_matrix/federation/v1/query/profile with wrong user ID, got '%s' want '%s'", userID, remoteUserID)
+						return
+					}
+					resBody, err := json.Marshal(struct {
+						Displayname string `json:"displayname"`
+						AvatarURL   string `json:"avatar_url"`
+					}{
+						remoteDisplayName, "",
+					})
+					if err != nil {
+						w.WriteHeader(500)
+						t.Fatalf("GET /_matrix/federation/v1/query/profile failed to marshal response: %s", err)
+						return
+					}
+					w.WriteHeader(200)
+					w.Write(resBody)
+				})).Methods("GET")
+
+				// query the display name which should do an outbound federation hit
+				unauthedClient := deployment.Client(t, "hs1", "")
+				res := unauthedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "v3", "profile", remoteUserID, "displayname"})
+				must.MatchResponse(t, res, match.HTTPResponse{
+					JSON: []match.JSON{
+						match.JSONKeyEqual("displayname", remoteDisplayName),
+					},
+				})
 			})
-			if err != nil {
-				w.WriteHeader(500)
-				t.Fatalf("GET /_matrix/federation/v1/query/profile failed to marshal response: %s", err)
-				return
-			}
-			w.WriteHeader(200)
-			w.Write(resBody)
-		})).Methods("GET")
-
-		// query the display name which should do an outbound federation hit
-		unauthedClient := deployment.Client(t, "hs1", "")
-		res := unauthedClient.MustDoFunc(t, "GET", []string{"_matrix", "client", "v3", "profile", remoteUserID, "displayname"})
-		must.MatchResponse(t, res, match.HTTPResponse{
-			JSON: []match.JSON{
-				match.JSONKeyEqual("displayname", remoteDisplayName),
-			},
 		})
-	})
-}
 
-func TestInboundFederationProfile(t *testing.T) {
-	deployment := Deploy(t, b.BlueprintAlice)
-	defer deployment.Destroy(t)
+		t.Run("Inbound", func(t *testing.T) {
+			// formerly TestInboundFederationProfile
+			// users needed: @alice:hs1
+			t.Parallel()
 
-	alice := deployment.Client(t, "hs1", "@alice:hs1")
+			srv := federation.NewServer(t, deployment,
+				federation.HandleKeyRequests(),
+			)
+			cancel := srv.Listen()
+			defer cancel()
 
-	srv := federation.NewServer(t, deployment,
-		federation.HandleKeyRequests(),
-	)
-	cancel := srv.Listen()
-	defer cancel()
+			// sytest: Non-numeric ports in server names are rejected
+			t.Run("Non-numeric ports in server names are rejected", func(t *testing.T) {
+				fedReq := gomatrixserverlib.NewFederationRequest(
+					"GET",
+					"hs1",
+					"/_matrix/federation/v1/query/profile"+
+						"?user_id=@user1:localhost:http"+
+						"&field=displayname",
+				)
 
-	// sytest: Non-numeric ports in server names are rejected
-	t.Run("Non-numeric ports in server names are rejected", func(t *testing.T) {
-		fedReq := gomatrixserverlib.NewFederationRequest(
-			"GET",
-			"hs1",
-			"/_matrix/federation/v1/query/profile"+
-				"?user_id=@user1:localhost:http"+
-				"&field=displayname",
-		)
+				resp, err := srv.DoFederationRequest(context.Background(), t, deployment, fedReq)
 
-		resp, err := srv.DoFederationRequest(context.Background(), t, deployment, fedReq)
+				must.NotError(t, "failed to GET /profile", err)
 
-		must.NotError(t, "failed to GET /profile", err)
+				must.MatchResponse(t, resp, match.HTTPResponse{
+					StatusCode: 400,
+				})
+			})
 
-		must.MatchResponse(t, resp, match.HTTPResponse{
-			StatusCode: 400,
-		})
-	})
+			// sytest: Inbound federation can query profile data
+			t.Run("Inbound federation can query profile data", func(t *testing.T) {
+				alice := deployment.Client(t, "hs1", "@alice:hs1")
+				const alicePublicName = "Alice Cooper"
 
-	// sytest: Inbound federation can query profile data
-	t.Run("Inbound federation can query profile data", func(t *testing.T) {
-		const alicePublicName = "Alice Cooper"
+				alice.MustDoFunc(
+					t,
+					"PUT",
+					[]string{"_matrix", "client", "v3", "profile", alice.UserID, "displayname"},
+					client.WithJSONBody(t, map[string]interface{}{
+						"displayname": alicePublicName,
+					}),
+				)
 
-		alice.MustDoFunc(
-			t,
-			"PUT",
-			[]string{"_matrix", "client", "v3", "profile", alice.UserID, "displayname"},
-			client.WithJSONBody(t, map[string]interface{}{
-				"displayname": alicePublicName,
-			}),
-		)
+				fedReq := gomatrixserverlib.NewFederationRequest(
+					"GET",
+					"hs1",
+					"/_matrix/federation/v1/query/profile"+
+						"?user_id=@alice:hs1"+
+						"&field=displayname",
+				)
 
-		fedReq := gomatrixserverlib.NewFederationRequest(
-			"GET",
-			"hs1",
-			"/_matrix/federation/v1/query/profile"+
-				"?user_id=@alice:hs1"+
-				"&field=displayname",
-		)
+				resp, err := srv.DoFederationRequest(context.Background(), t, deployment, fedReq)
 
-		resp, err := srv.DoFederationRequest(context.Background(), t, deployment, fedReq)
+				must.NotError(t, "failed to GET /profile", err)
 
-		must.NotError(t, "failed to GET /profile", err)
-
-		must.MatchResponse(t, resp, match.HTTPResponse{
-			StatusCode: 200,
-			JSON: []match.JSON{
-				match.JSONKeyEqual("displayname", alicePublicName),
-			},
+				must.MatchResponse(t, resp, match.HTTPResponse{
+					StatusCode: 200,
+					JSON: []match.JSON{
+						match.JSONKeyEqual("displayname", alicePublicName),
+					},
+				})
+			})
 		})
 	})
 }
