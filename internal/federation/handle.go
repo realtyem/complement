6eaa7edbb64f29a1e9b7c04670f11857bed872c5
Jason Little <realtyem@gmail.com>
2023-01-08 01:26:05 -0600

    [handle.go] Upgrade mutex locks to RWMutex.

--- internal/federation/handle.go
+++ internal/federation/handle.go
@@ -31,7 +31,9 @@ func MakeJoinRequestsHandler(s *Server, w http.ResponseWriter, req *http.Request
 	userID := vars["userID"]
 	roomID := vars["roomID"]
 
+	s.mutex.RLock()
 	room, ok := s.rooms[roomID]
+	s.mutex.RUnlock()
 	if !ok {
 		w.WriteHeader(404)
 		w.Write([]byte("complement: HandleMakeSendJoinRequests make_join unexpected room ID: " + roomID))
@@ -148,7 +150,9 @@ func SendJoinRequestsHandler(s *Server, w http.ResponseWriter, req *http.Request
 	vars := mux.Vars(req)
 	roomID := vars["roomID"]
 
+	s.mutex.RLock()
 	room, ok := s.rooms[roomID]
+	s.mutex.RUnlock()
 	if !ok {
 		w.WriteHeader(404)
 		w.Write([]byte("complement: HandleMakeSendJoinRequests send_join unexpected room ID: " + roomID))
@@ -266,7 +270,9 @@ func HandleInviteRequests(inviteCallback func(*gomatrixserverlib.Event)) func(*S
 			}
 
 			// Sign the event before we send it back
+			s.mutex.RLock()
 			signedEvent := inviteRequest.Event().Sign(s.serverName, s.KeyID, s.Priv)
+			s.mutex.RUnlock()
 
 			// Send the response
 			res := map[string]interface{}{
