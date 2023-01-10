e5ed849ce48f88a136feb7f85bb2c4e79665fa8d
Jason Little <realtyem@gmail.com>
2023-01-10 17:04:22 -0600

    [federation_presence_test.go] Clean rooms/users to always use a fresh one so no presence contamination from the blueprint.

--- tests/federation_presence_test.go
+++ tests/federation_presence_test.go
@@ -13,15 +13,25 @@ func TestRemotePresence(t *testing.T) {
 	deployment := Deploy(t, b.BlueprintFederationOneToOneRoom)
 	defer deployment.Destroy(t)
 
+	alicePassword := "alice_secret_password"
+	bobPassword := "bob_lame_password"
 
 	// sytest: Presence changes are also reported to remote room members
 	t.Run("Presence changes are also reported to remote room members", func(t *testing.T) {
-		alice := deployment.Client(t, "hs1", "@alice:hs1")
-		bob := deployment.Client(t, "hs2", "@bob:hs2")
+		//alice := deployment.Client(t, "hs1", "@alice:hs1")
+		//bob := deployment.Client(t, "hs2", "@bob:hs2")
+		alice := deployment.RegisterUser(t, "hs1", "testRemotePresence01Alice", alicePassword, false)
+		bob := deployment.RegisterUser(t, "hs2", "testRemotePresence01Bob", bobPassword, false)
+
+		roomID := alice.CreateRoom(t, map[string]interface{}{
+			"preset":     "public_chat",		})
+
+		bob.JoinRoom(t, roomID, []string{"hs1"})
+
 		_, bobSinceToken := bob.MustSync(t, client.SyncReq{TimeoutMillis: "0"})
 
 		statusMsg := "Update for room members"
-		alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "presence", "@alice:hs1", "status"},
+		alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "presence", alice.UserID, "status"},
 			client.WithJSONBody(t, map[string]interface{}{
 				"status_msg": statusMsg,
 				"presence":   "online",
@@ -36,11 +46,19 @@ func TestRemotePresence(t *testing.T) {
 	})
 	// sytest: Presence changes to UNAVAILABLE are reported to remote room members
 	t.Run("Presence changes to UNAVAILABLE are reported to remote room members", func(t *testing.T) {
-		alice := deployment.Client(t, "hs1", "@alice:hs1")
-		bob := deployment.Client(t, "hs2", "@bob:hs2")
+		//alice := deployment.Client(t, "hs1", "@alice:hs1")
+		//bob := deployment.Client(t, "hs2", "@bob:hs2")
+		alice := deployment.RegisterUser(t, "hs1", "testRemotePresence02Alice", alicePassword, false)
+		bob := deployment.RegisterUser(t, "hs2", "testRemotePresence02Bob", bobPassword, false)
+
+		roomID := alice.CreateRoom(t, map[string]interface{}{
+			"preset":     "public_chat",		})
+
+		bob.JoinRoom(t, roomID, []string{"hs1"})
+
 		_, bobSinceToken := bob.MustSync(t, client.SyncReq{TimeoutMillis: "0"})
 
-		alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "presence", "@alice:hs1", "status"},
+		alice.MustDoFunc(t, "PUT", []string{"_matrix", "client", "v3", "presence", alice.UserID, "status"},
 			client.WithJSONBody(t, map[string]interface{}{
 				"presence": "unavailable",
 			}),
